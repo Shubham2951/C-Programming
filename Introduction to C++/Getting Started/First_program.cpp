0fc10d4fad6a054342d357a4718d28c7a0a4076b
Shubham2951 <58352244+Shubham2951@users.noreply.github.com>
2021-03-28 15:22:57 +0530

    Create First_program.cpp

---
+++ Introduction to C++/Getting Started/First_program.cpp
@@ -0,0 +1,17 @@
+Problem: print "Hello world!"
+ ************************************************************************** 
+ Program:
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
+
+***************************************************************************
+Output:
+Hello world!
